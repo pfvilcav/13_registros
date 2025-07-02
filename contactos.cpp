d5a01189af14ad84e6fdcc996d2e3de4d8db8c2c
pfvilcav <pfvilcav@unjbg.edu.pe>
2025-07-01 22:49:31 -0500

    Se arreglo un poco el código para volverlo mas legible y poderlo modularizar.

--- contactos.cpp
+++ contactos.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#define MAX 10
 using namespace std;
 
 struct correo{
@@ -16,11 +17,12 @@ struct contactoEmail{
 void leerCorreo(correo &, string, string);
 void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
+void agregarContacto(contactoEmail[], contactoEmail, int &);
 
 int main(){
     int n, op;
     string nom, user, domain, filtro;
-    char sex;
+    char sex, SN;
     int edad;
     correo email;
     contactoEmail cont, lista[100];
@@ -35,36 +37,38 @@ int main(){
         cout<<"Elige una opcion: "; cin>>op;
         switch(op){
             case 1:
-                cout<<"Ingrese los datos de un usuario: "<<endl;
-                cin.ignore();
-                cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
-                cout<<"Ingrese el sexo (M/F): "; cin>>sex;
-                cout<<"Ingrese la edad: "; cin>>edad;
-                cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
-                cout<<"\tIngrese el usuario: "; cin>>user;
-                cout<<"\tIngrese el dominio: "; cin>>domain;
-                
-                leerCorreo(email,user,domain);
-                leerContacto(cont,nom,sex,edad,email);
-                //imprimeContacto(cont);
+                if(n < MAX){
+                    cout<<"Ingrese los datos de un usuario:"<<endl;
+                    cin.ignore();
+                    cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
+                    cout<<"Ingrese el sexo (M/F): "; cin>>sex;
+                    cout<<"Ingrese la edad: "; cin>>edad;
+                    cout<<"Ingrese el correo electronico (usuario@dominio):"<<endl;
+                    cout<<"\tIngrese el usuario: "; cin>>user;
+                    cout<<"\tIngrese el dominio: "; cin>>domain;
+                    
+                    leerCorreo(email,user,domain);
+                    leerContacto(cont,nom,sex,edad,email);
 
-                lista[n] = cont;
-                n++;
-                system("pause");
-                break;
-            case 2:
-                for(int i = 0; i < n; i++){
-                    cout<<"Contacto #"<<i<<endl;
-                    imprimeContacto(lista[i]);
-                    cout<<endl;
+                    cout<<"\nHa ingresado la siguiente informacion:"<<endl;
+                    imprimeContacto(cont);
+                    cout<<"\nEsta informacion es correcta? (S/N): "; cin>>SN;
+                    if(SN == 'S' || SN == 's'){
+                        agregarContacto(lista,cont,n);
+                        cout<<"Registro exitoso!"<<endl;
+                    }else{
+                        cout<<"Se cancelo la operacion"<<endl;
+                    }
+                }else{
+                    cout<<"Limite de contactos alcanzado"<<endl;
                 }
                 system("pause");
                 break;
-            case 3:
-                cout<<"Ingrese el dominio a filtrar: "; 
-                cin>>filtro;
-                for (int i = 0; i < n; i++) {
-                    if (lista[i].email.domain==filtro) {
+            case 2:
+                if(n == 0){
+                    cout<<"Aun no se han registrado contactos"<<endl;
+                }else{
+                    for(int i = 0; i < n; i++){
                         cout<<"Contacto #"<<i+1<<endl;
                         imprimeContacto(lista[i]);
                         cout<<endl;
@@ -72,8 +76,35 @@ int main(){
                 }
                 system("pause");
                 break;
+            case 3:
+                if(n == 0){
+                    cout<<"Aun no se han registrado contactos"<<endl;
+                }else{
+                    cout<<"Ingrese el dominio a filtrar: "; cin>>filtro;
+                    bool encontrado = false;
+                    for(int i = 0; i < n; i++){
+                        if(lista[i].email.domain == filtro){
+                            cout<<"Contacto #"<<i+1<<endl;
+                            imprimeContacto(lista[i]);
+                            cout<<endl;
+                            encontrado = true;
+                        }
+                    }
+                    if(!encontrado){
+                        cout<<"No hay contactos con ese dominio"<<endl;
+                    }
+                }
+                system("pause");
+                break;
             case 0:
-                cout<<"Esta seguro de salir? (S/N): ";
+                cout<<"Esta seguro de salir? (S/N): "; cin>>SN;
+                if(SN != 'S' && SN != 's'){
+                    op = -1;
+                    cout<<"Se cancelo la operacion"<<endl;
+                }else{
+                    cout<<"Hasta la proxima!"<<endl;
+                }
+                system("pause");
                 break;
             default:
                 cout<<"Opcion no valida!"<<endl;
@@ -101,4 +132,9 @@ void imprimeContacto(contactoEmail &c){
     cout<<"Sexo: "<<c.sex<<endl;
     cout<<"Edad: "<<c.edad<<endl;
     cout<<"Email: "<<c.email.user<<"@"<<c.email.domain<<endl;
+}
+
+void agregarContacto(contactoEmail lista[], contactoEmail cE, int &cant){
+    lista[cant] = cE;
+    cant++;
 }
